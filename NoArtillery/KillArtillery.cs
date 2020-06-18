73b771aa2df2d5794f5f273c8ffabc8911b6b550
Walentin <walentinlamonos@googlemail.com>
2020-06-18 22:46:48 +0200

    Kill Artillery mod

---
+++ NoArtillery/KillArtillery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class KillArtillery : MonoBehaviour
+{
+    private readonly string[] artilleryNames =
+    {
+        "Cannon_4Pdr",
+        "Cannon_6Pdr",
+        "Cannon_18Pdr",
+        "Cannon_24Pdr",
+        "Cannon_FieldGun_9PDR",
+        "Carronade",
+        "CoastalCannon_36Pdr",
+        "Longgun_RotatingCannonCarriage",
+        "Mortar",
+        "MovableCannon_FieldGun_9PDR",
+        "Swivlegun",
+        "Rocket_Moveable_Usable",
+        "Rocket_launcher_Gunboat"
+    };
+
+    private void Awake()
+    {
+        try
+        {
+            GameObject[] gos = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+
+            for (int i = 0; i < gos.Length; i++)
+            {
+                var goName = gos[i].name;
+                if (goName.IndexOf("(Clone)", StringComparison.OrdinalIgnoreCase) >= 0) // Prefabs get "(Clone)" appended these were spawned using overrides, so keep them
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < artilleryNames.Length; j++)
+                {
+                    if (goName.IndexOf(artilleryNames[j], StringComparison.OrdinalIgnoreCase) >= 0) // Kill anything that's in the array, leave noone alive!
+                    {
+                        Destroy(gos[i]);
+                        break;
+                    }
+                }
+            }
+            Destroy(this);
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
