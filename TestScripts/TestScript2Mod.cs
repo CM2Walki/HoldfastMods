cf7d82d289126860f11ce6285190bd25e3ff3c25
Dylan Abela <dylan.abela@gmail.com>
2022-09-13 14:15:36 +0200

    Updated with 2.7 support

--- TestScripts/TestScript2Mod.cs
+++ TestScripts/TestScript2Mod.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class TestScript2Mod : IHoldfastSharedMethods2
 {
-    public void OnOfficerOrderStart(int officerPlayerId, OfficerOrderType officerOrderType, Vector3 orderPosition, float orderRotationY, int voicePhraseRandomIndex)
+    public void OnOfficerOrderStart(int officerPlayerId, HighCommandOrderType highCommandOrderType, Vector3 orderPosition, float orderRotationY, int voicePhraseRandomIndex)
     {
-        Debug.LogFormat("OnOfficerOrderStart {0} {1}", officerPlayerId, officerOrderType);
+        Debug.LogFormat("OnOfficerOrderStart {0} {1}", officerPlayerId, highCommandOrderType);
     }
 
-    public void OnOfficerOrderStop(int officerPlayerId, OfficerOrderType officerOrderType)
+    public void OnOfficerOrderStop(int officerPlayerId, HighCommandOrderType highCommandOrderType)
     {
-        Debug.LogFormat("OnOfficerOrderStop {0} {1}", officerPlayerId, officerOrderType);
+        Debug.LogFormat("OnOfficerOrderStop {0} {1}", officerPlayerId, highCommandOrderType);
     }
 
     public void OnPlayerPacket(int playerId, byte? instance, Vector3? ownerPosition, double? packetTimestamp, Vector2? ownerInputAxis, float? ownerRotationY, float? ownerPitch, float? ownerYaw, PlayerActions[] actionCollection, Vector3? cameraPosition, Vector3? cameraForward, ushort? shipID, bool swimming)
